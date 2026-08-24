00a593a1b3c8213b5ef4bb8049b50b0e087476d5
KonHouse <166307304+KonHouse@users.noreply.github.com>
2026-08-24 17:27:47 +0200

    chore: bump app version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.example.valomobile"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "VALORANT_API_KEY", "\"$valorantApiKey\"")
